5447e539d9f6b31fe16aea3de7ff851e85080521
Andrew <andrew.iacco@mavinworks.com>
2017-07-18 10:30:40 -0400

    Fixed javascript scroll height issue, cleaning up html, lightbox, added background svg.

--- javascripts/misc.js
+++ javascripts/misc.js
@@ -1,54 +1,45 @@
 // AI: misc javascripts I needed.
 
-// ========== Scrollmagic and greensock code here. ==========
 $(document).ready(function() {
 	
-	//Set variable for drop height
-	dropHeight = $('#main-content').height(); 
-	//console.log($('#main-content').height());
-	
-	//initialize controller
-	var controller = new ScrollMagic.Controller();
-	
-	//build drop tween
-	var drop = TweenMax.to("#drop", 1, {y: dropHeight, ease: Linear.easeIn});
+	setTimeout(function(){
+		var dropHeight = $('#main-content').outerHeight(true); //get height the drop has to fall
+		
+		var controller = new ScrollMagic.Controller(); //initialize controller
+		
+		var dropTween = TweenMax.to("#drop", 1, {y: dropHeight, ease: Linear.easeIn}); //build drop tween
 
-	//build a scene for the drop
-	var drop = new ScrollMagic.Scene({
-		triggerElement: 'body', 
-		duration:dropHeight, //duration is height of main content div
-		triggerHook: 0
-	})
-	.setTween(drop)
-	//.addIndicators({name: 'drop'})
-	.setClassToggle("#drop", "show-it")
-	.addTo(controller);
-	
-	// ========== loop through each arrow and add fade in class ==========
-	$('.arrow').each(function(){
-		// build a scene 
-		var scene = new ScrollMagic.Scene({
-			triggerElement: this,
-			triggerHook: 'onLeave',
-			offset:-200, //do animation this many pixels before each arrow leaves screen
-			reverse: false //keep arrows showing after you've showed them
+		var dropScroll = new ScrollMagic.Scene({
+			triggerElement: 'body', 
+			duration: dropHeight, //duration is height of main content div
+			triggerHook: 0
 		})
-		.setClassToggle(this, 'fade-in')
+		.setTween(dropTween)
+		.setClassToggle("#drop", "show-it")
 		.addTo(controller);
-	});
-
-	// ========== Change colors when paint buckets clicked. ==========
-
-	$('.paint-bucket').click(function(){
+		
+		$('.arrow').each(function(){ //loop through each arrow and add fade in class
+			var showArrow = new ScrollMagic.Scene({
+				triggerElement: this,
+				triggerHook: 'onLeave',
+				offset:-200, //start animation this many pixels before each arrow leaves the screen
+				reverse: false //keep arrows showing after you've showed them
+			})
+			.setClassToggle(this, 'fade-in')
+			.addTo(controller);
+		});
+		
+	}, 100); //wait for page/js to finish loading before calculating height.
+	
+	$('.paint-bucket').click(function(){ //change colors when paint buckets clicked.
 		var paintColor = $(this).css('color'); 	//get paint bucket color
-		//console.log(paintColor);
-	    $('#puddle path').css('fill', paintColor); 	//set puddle color
+		$('#puddle').children('path').css('fill', paintColor); //set puddle color
 		$('#page-puddle').css('border-color', paintColor); //set bottom border color
-		$('#drop path').css('fill', paintColor); 	//set droplet color
+		$('#drop').children('path').css('fill', paintColor); 	//set droplet color
 		$('.logo-color').css('fill', paintColor); 	//set logo color
-		$('.number-holder span').css('background-color', paintColor); 	//set step color
-		$('.arrow path').css('stroke', paintColor); //set arrow color
+		$('.number-holder').children('span').css('background-color', paintColor); 	//set step color
+		$('.arrow').find('path').css('stroke', paintColor); //set arrow color
 		return false;
 	});
-
+	
 });
