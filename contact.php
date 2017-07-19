498aecde4200987ef9098aa92c576461d90ab9a0
Andrew <andrew.iacco@mavinworks.com>
2017-07-19 11:19:28 -0400

    Contact form in lightbox, php script to send contact email, form styles.

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+
+	if(isset($_POST['url']) && $_POST['url'] == ''){ // if the url field is empty they're not a robot
+
+		$sendTo = 'andrew@iacco.us';
+	
+		$body = "iacco.us web contact form:
+		Name:  $_POST[name]
+		Company: $_POST[company]
+		Message: $_POST[message]
+		Contact: $_POST[email]";
+
+		if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
+		  $headers = "From: $_POST[email]";
+		} else {
+		  $headers = "From: $sendTo";
+		}
+	
+		mail($sendTo, 'Iacco.us Contact Form', $body, $headers );
+
+	}
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Thank you!</title>
+	<link href="/stylesheets/style.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body class="background-texture">
+	<div class="container padbox">
+		<h1>Thanks</h1>
+		<p>We'll get back to you as soon as possible.</p>
+		<p><a href="/" class="default-link">&larr; Go back to iacco.us</a></p>
+	</div>
+</body>
+</html>
